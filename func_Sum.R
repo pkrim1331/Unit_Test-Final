9929cad72512b0dcde16c81d598cb4660b5f939c
Patrick Krim <patrick@Patricks-MacBook-Pro-5.local>
2018-04-26 09:22:19 -0500

    updated data

--- func_Sum.R
+++ func_Sum.R
@@ -8,7 +8,17 @@
 #A vector of values of total precipitation for each weather station for the year
 
 #apply function use columns (second dimensions) with sum as function
-apply(eet, 2, sum, trim = .008)
+apply(eet, 1, sum)
 
+gg<-colSums(Filter(is.numeric, eet), na.rm=T)
+hist(gg)
+apply(Filter(is.numeric, eet), na.rm=T, sum)
+
+#x <- cbind(eet$x = 3, x2 = c(4:1, 2:5))
+#dimnames(x)[[1]] <- letters[1:8]
+apply(x, 2, mean, trim = .2)
+col.sums <- apply(x, 2, sum)
+row.sums <- apply(x, 1, sum)
+rbind(cbind(x, Rtot = row.sums), Ctot = c(col.sums, sum(col.sums)))
 
 
