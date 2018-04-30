d97ece69c3491d42e54951b4f51888f62b38527f
Patrick Krim <patrick@Patricks-MacBook-Pro-5.local>
2018-04-30 11:22:11 -0500

    removal of 3 first funcs.

--- Plot_func.R
+++ Plot_func.R
@@ -1,6 +1,6 @@
 
 yr<-c(1982, 1997, 2015, 1983, 1998, 2016)
-barplot(all[[1]], legend = rownames(all), xlab = 'years', ylab = 'Total precipitation in CA(in.)', )
+barplot(all[[1]], legend = rownames(all), xlab = 'Years', ylab = 'Total precipitation in CA(in.)', )
 
 
 
