0f0a1a91a1cd8a9bb40461e18ed54e7d19c3872e
Patrick Krim <patrick@Patricks-MacBook-Pro-5.local>
2018-05-07 14:06:31 -0500

    added a plot to show data

--- Plot_func.R
+++ Plot_func.R
@@ -5,9 +5,17 @@ yr<-c(1982, 1983, 1997, 1998, 2015, 2016)
 avgDataframe<-as.data.frame(avg, row.names = yr, col.names = "Avg daily pcpn", sep = "")
 print(avgDataframe)
 
-scatter.smooth(avgDataframe, span = 2/3, degree = 1, family = "symmetric", xlab = "years", ylab = "pcpn", evaluation = 6)
+#Average daily precipitation for each year, alternating El Nino year and non-El Nino year
+scatter.smooth(avgDataframe, span = 2/3, degree = 1, family = "symmetric", xlab = "Years", ylab = "Avg Daily Precipitation (in.)", evaluation = 6)
 
+totDataframe<- as.data.frame(total, row.names = yr, col.names = "Total Precipitation", sep = "")
+print(totDataframe)
 
+#Total precipitation for each year also alternating El Nino years and non-El Nino years
+scatter.smooth(totDataframe, span = 2/3, degree = 1, family = "symmetric", xlab = "Years", ylab = "Total Prcipitation (in.)")
 
+Tpcpn<- as.matrix(totDataframe)
+barplot(Tpcpn, width = 1, names.arg = yr, space = 1, axisnames = T, axis.lty = 6, beside = T, ylab = "Total Precipitation (in.)", xlab = "Years")
+?barplot
 
-
+?scatter.smooth
